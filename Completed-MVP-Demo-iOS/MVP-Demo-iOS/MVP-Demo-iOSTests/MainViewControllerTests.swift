a1809d9de71b91d067cc3857b622dc887499a7ce
Ikmal Azman <opikmalazman@gmail.com>
2022-07-03 17:01:59 +0800

    add unit test bundle

---
+++ Completed-MVP-Demo-iOS/MVP-Demo-iOS/MVP-Demo-iOSTests/MainViewControllerTests.swift
@@ -0,0 +1,9 @@
+@testable import MVP_Demo_iOS
+import XCTest
+
+final class MainViewControllerTests: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in MainViewControllerTests")
+    }
+}
