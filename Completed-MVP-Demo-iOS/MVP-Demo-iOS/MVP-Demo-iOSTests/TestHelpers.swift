e47a9cbe644d4d0bc2f7f7f31bea2556bf8b324d
Ikmal Azman <opikmalazman@gmail.com>
2022-07-04 09:06:31 +0800

    add helpers for unit test

--- Completed-MVP-Demo-iOS/MVP-Demo-iOS/MVP-Demo-iOSTests/TestHelpers.swift
+++ Completed-MVP-Demo-iOS/MVP-Demo-iOS/MVP-Demo-iOSTests/TestHelpers.swift
@@ -1,9 +1,36 @@
 @testable import MVP_Demo_iOS
-import XCTest
+import UIKit
 
-final class TestHelpers: XCTestCase {
 
-    func test_zero() throws {
-        XCTFail("Tests not yet implemented in TestHelpers")
-    }
+func tap(_ button : UIBarButtonItem) {
+    _ = button.target?.perform(button.action, with: nil)
+}
+
+/// Ask UIKit to execute registered events immediately
+func executeRunLoop() {
+    RunLoop.main.run(until: Date())
+}
+
+//MARK: - UITableViewSource Helper
+func numberOfRowsInSection(_ tableView : UITableView, section : Int = 0) -> Int? {
+    return tableView.dataSource?.tableView(tableView, numberOfRowsInSection: section)
+}
+
+func cellForRowAt(_ tableView : UITableView, row : Int, section : Int = 0) -> UITableViewCell? {
+    let indexPath = IndexPath(row: row, section: section)
+    return tableView.dataSource?.tableView(tableView, cellForRowAt: indexPath)
+}
+
+func createLeagueData(_ name : String) -> League {
+    return League(name: name)
+}
+
+
+//MARK: - MainPresenter Helper
+func presentActionWhenRefresh(_ presenter: MainPresenter) {
+    presenter.delegate?.presentActionWhenRefresh(presenter)
+}
+
+func presentListOfLeague(_ presenter: MainPresenter, data : [League]) {
+    presenter.delegate?.presentListOfLegue(presenter, data: data)
 }
