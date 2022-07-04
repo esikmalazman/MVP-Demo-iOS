4fd0a8c50be3446b2a1a25f468ce371ba201eaaa
Ikmal Azman <opikmalazman@gmail.com>
2022-07-04 09:03:49 +0800

    add unit test bundle

---
+++ Completed-MVP-Demo-iOS/MVP-Demo-iOS/MVP-Demo-iOSTests/MainPresenterSpy.swift
@@ -0,0 +1,8 @@
+//
+//  MainPresenterSpy.swift
+//  MVP-Demo-iOSTests
+//
+//  Created by Ikmal Azman on 04/07/2022.
+//
+
+import Foundation
---
+++ Completed-MVP-Demo-iOS/MVP-Demo-iOS/MVP-Demo-iOSTests/TestHelpers.swift
@@ -0,0 +1,9 @@
+@testable import MVP_Demo_iOS
+import XCTest
+
+final class TestHelpers: XCTestCase {
+
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in TestHelpers")
+    }
+}
